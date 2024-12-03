fa02433b419ec21c202ef730443c82baf1fc675c
caiocesarps2 <caiocesarpenha@gmail.com>
2024-12-02 22:45:03 -0300

    style: alteração no HTML e no CSS e modificando algumas coisas no JS

--- Website/js/book.js
+++ Website/js/book.js
@@ -1,28 +1,35 @@
+let livros = [];
+
 document.getElementById('form-livro').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const title = document.getElementById('titulo-livro').value;
-    const author = document.getElementById('autor-livro').value;
-    const price = document.getElementById('preco-livro').value;
+  e.preventDefault();
 
-    try {
-      const response = await fetch('http://localhost:3000/books', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, author, price }),
-      });
+  const title = document.getElementById('titulo-livro').value;
+  const author = document.getElementById('autor-livro').value;
+  const price = document.getElementById('preco-livro').value;
 
-      if (response.ok) {
-        alert('Cadastro do Livro realizado com sucesso!');
-        console.log({title, author, price})
-      } else {
-        throw new Error();
-      }
-    } catch {
-      alert('Tente realizar novamente o cadastro do livro.');
+  try {
+    const response = await fetch('http://localhost:3000/books', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title, author, price }),
+    });
+
+
+    if (response.ok) {
+      livros.push({ title, author, price });
+      console.log('Livros cadastrados:', livros);
+      alert('Cadastro do Livro realizado com sucesso!');
+    } else {
+      throw new Error('Erro no cadastro');
     }
-  });
+  } catch (error) {
+    console.error(error);
+    alert('Tente realizar novamente o cadastro do livro.');
+  }
+});
 
-  document.getElementById('form-livro').addEventListener("reset", (e) =>{
-    e.preventDefault();
-    window.location.href = 'login.html';
-  })
+// Evento de reset do formulário
+document.getElementById('form-livro').addEventListener("reset", (e) => {
+  e.preventDefault();
+  window.location.href = 'login.html';
+});
